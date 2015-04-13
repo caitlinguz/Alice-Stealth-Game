9d0ab3ee19f94d588ca1dd68d30e66fccc45160b
Caitlin Guzman <cg1656@nyu.edu>
2015-04-13 10:31:36 -0400

    Pretty much most of the things

---
+++ Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCMovement : MonoBehaviour {
+	
+	public float speed = 0.5f;
+
+	public GameObject player;
+	
+	// FixedUpdate is called once per physics frame / fixed timestep
+	void FixedUpdate () {
+		
+		// always move forward
+		GetComponent<Rigidbody>().AddForce ( transform.forward * speed, ForceMode.VelocityChange );
+		
+		// generate ray
+		Ray ray = new Ray ( transform.position, transform.forward );
+		
+		// actually shoot raycast
+		if ( Physics.Raycast ( ray, 3f ) ) {
+			// if the raycast hits something, then rotate
+			transform.Rotate ( 0f, 95f, 0f );
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// generate a ray before shooting a raycast
+		Ray mouseCursorRay = Camera.main.ScreenPointToRay ( Input.mousePosition );
+
+		RaycastHit rayCastDestination = new RaycastHit ();
+
+		if ( Physics.Raycast ( mouseCursorRay, out rayCastDestination, 1000f ) ) {
+
+			//	set (moveDirection) = vector from (current position) to (destination)
+			var moveDirection = rayCastDestination.point - transform.position;
+
+			//	normalize (moveDirection)
+			moveDirection = moveDirection.normalized;
+
+			//	addforce in (moveDirection)
+			rigidbody.AddForce ( moveDirection * speed);
+		}
+	}
+}
+
+//Update:
+//If player clicks...
+//	Raycast from mouse cursor.
+//	remember RaycastHit impact point as (destination)
+//	set (moveDirection) = vector from (current position) to (destination)
+//	normalize (moveDirection)
+//	addforce in (moveDirection)
